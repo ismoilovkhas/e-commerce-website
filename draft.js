689fdc36edf391309c623faec649179e44ed32a4
ismoilovkhas <87958287+ismoilovkhas@users.noreply.github.com>
2021-07-27 22:54:30 -0400

    Add files via upload

--- draft.js
+++ draft.js
@@ -115,7 +115,7 @@ function render_cart() {
 ///////////////////////////////////add to cart function              hurrahhhh!!!
 var pressherebtn= document.getElementsByClassName('c')
 var erased_cart_innnerhtml= ` <tr id="my_cart_head">
-<th></th>
+<th>ID</th>
 <th></th>
 <th>PRODUCT NAME</th>
 <th><div></div>PRICE</th>
@@ -235,4 +235,30 @@ function hmmm() {
 }    
   
 }  
-       
+ 
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("cart_image");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
