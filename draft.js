57bdac6636d68e23a1516669901ff5e3dae85877
ismoilovkhas <87958287+ismoilovkhas@users.noreply.github.com>
2021-08-12 23:37:12 -0400

    Add files via upload

--- draft.js
+++ draft.js
@@ -6,42 +6,42 @@ var products = [
     img: "101.jpeg",
     name: "International Front Lid 4 Wheeled Carry-On",
     price: 595,
-    quantity: 1
+    quantity: 0
   },
   {
     id: 102,
     img: "102.jpeg",
     name: "International Carry-On",
     price: 750,
-    quantity: 1
+    quantity: 0
   },
   {
     id: 103,
     img: "103.jpeg",
     name: "Oxford Compact Carry-On",
     price: 595,
-    quantity: 1
+    quantity: 0
   },
   {
     id: 104,
     img: "104.jpeg",
     name: "Large Split 2 Wheeled Duffel",
     price: 895,
-    quantity: 1
+    quantity: 0
   },
   {
     id: 105,
     img: "105.jpeg",
     name: "TUMI Brief Pack",
     price: 525,
-    quantity: 1
+    quantity: 0
   },
   {
     id: 106,
     img: "106.jpeg",
     name: "Kelley Sling",
     price: 275,
-    quantity: 1
+    quantity: 0
   }
 ]
 
@@ -64,11 +64,13 @@ function renderboxx() {
   
   `;
   ull.appendChild(lii);
+  localStorage.setItem(JSON.stringify(element.id),JSON.stringify(element.name))
 }
 
 
 }
 renderboxx()
+
 /////////////////////////render shopping cart function
 var shopping_cart=[]
   
@@ -84,45 +86,33 @@ function render_cart() {
     row.innerHTML = `
     <tr class="cart_row">
     <th class="cart_item"> ${in_cart_item.id}</th>
-      <th> <img src= ${in_cart_item.img} width="100" height="100" class= "product_image_zoom"></th>
-      <th>${in_cart_item.name}</th>
+      <th> <img src= ${in_cart_item.img} width="150" height="150" class= "product_image_zoom"></th>
+      <th >${in_cart_item.name}</th>
       <th class="unit_price">${in_cart_item.price} $</th>
       <th><button type="button" class="cart_btn_plus">+</button></th>
        <th class="cart_item_quantity">${in_cart_item.quantity}</th>
        <th><button type="button" class="cart_btn_minus">-</button></th>
        <th class="subtotal_result">${subtotal=in_cart_item.price* in_cart_item.quantity } </th>
+       <th >$ </th>
      </tr>
       `
        
   }
-  
-
-//   let roww= document.createElement('tr')
-//   ul4.appendChild(roww);
-//   roww.innerHTML= `<tr id="my_cart_bottom">
-//   <th></th>
-//   <th>YOUR TOTAL AMOUNT IS </th>
-//   <th></th>
-//   <th></th>
-//   <th></th>
-//   <th></th>
-//   <th></th>
-//   <th id="total_result"> $</th>
-// </tr>`
-
+  cart_items_qnty()
 
 }
 ///////////////////////////////////add to cart function              hurrahhhh!!!
 var pressherebtn= document.getElementsByClassName('c')
-var erased_cart_innnerhtml= ` <tr id="my_cart_head">
-<th>ID</th>
+var erased_cart_innnerhtml= `
+<tr id="my_cart_head">
+<th>#</th>
 <th></th>
-<th>PRODUCT NAME</th>
-<th><div></div>PRICE</th>
+<th>NAME</th>
+<th id="price_cart_head">PRICE</th>
 <th></th>
 <th>QUANTITY</th>
 <th></th>
-<th>SUBTOTAL</th>
+<th id="subtotal_cart_head">SUBTOTAL</th>
 </tr>`
 
 for (let i = 0; i < pressherebtn.length; i++) {
@@ -132,9 +122,11 @@ for (let i = 0; i < pressherebtn.length; i++) {
     let item_from_base= products[i]
     let obj= shopping_cart.find(o => o.id == item_from_base.id)
     var cart_add_btn= document.getElementsByClassName('cart_btn_plus')
-    var cart_item_quantity_btn= document.getElementsByClassName('cart_item_quantity')
+    // var cart_item_quantity_btn= document.getElementsByClassName('cart_item_quantity')
     if (obj==undefined) {
       shopping_cart.push(item_from_base)
+      objInd = shopping_cart.findIndex((ob => ob.id == item_from_base.id));
+      shopping_cart[objInd].quantity =  1
       ul4.innerHTML=erased_cart_innnerhtml
 
   render_cart()
@@ -143,7 +135,7 @@ hmmm()
 plus_btn_function()
 
 minus_btn_function()
-
+cart_items_qnty()
     } else {
       objIndex = shopping_cart.findIndex((ob => ob.id == item_from_base.id));
       shopping_cart[objIndex].quantity =  shopping_cart[objIndex].quantity+1
@@ -155,7 +147,7 @@ hmmm()
     plus_btn_function()
     
     minus_btn_function()
-    
+    cart_items_qnty()
     }
 
 }
@@ -176,7 +168,7 @@ hmmm()
     hmmm()
     plus_btn_function()
     minus_btn_function()
-    
+    // cart_items_qnty()
       }
     }
     }
@@ -196,10 +188,14 @@ hmmm()
         for( var i = 0; i < shopping_cart.length; i++){ 
     
           if ( shopping_cart[i].quantity == 0) { 
+            cart_items_qnty()
       
               shopping_cart.splice(i, 1); 
+              
               let total_result_tag=document.getElementById ('total_result')
-              total_result_tag.innerHTML="0 $"
+              total_result_tag.innerHTML="0 "
+              
+              // cart_items_qnty()
 
           }
       
@@ -211,18 +207,14 @@ hmmm()
       render_cart()
       hmmm()
       minus_btn_function()
+      
       plus_btn_function()
+     
 
         }
       }
       }
       
-    //  let get_cart_image= document.getElementById('cart_image').addEventListener('click', display_cart)
-        // function display_cart(){
-        //   window.alert('hello')
-        //   let get_textik= document.getElementById('my_cart')
-        //  get_textik.classList.toggle("mystyle");
-        // }
 function hmmm() {
   let subtotal_result_tag= document.getElementsByClassName('subtotal_result')
   let total_result_tag=document.getElementById ('total_result')
@@ -230,7 +222,6 @@ function hmmm() {
   for (let index = 0; index < subtotal_result_tag.length; index++) {
     let element= Number( subtotal_result_tag[index].innerHTML)
     total= total + element
-    // window.alert(total)
     total_result_tag.innerHTML=total +"$"
 }    
   
@@ -247,8 +238,13 @@ var btn = document.getElementById("cart_image");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
+btn.onclick = function open_modal() {
+  let cart_length= document.getElementById('cart_items_counter')
+  if (Number(cart_length.innerHTML)>0) {
+    modal.style.display = "block";
+  } 
+  
+  
 }
 
 // When the user clicks on <span> (x), close the modal
@@ -262,3 +258,58 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+function cart_items_qnty() {
+  let cart_length= document.getElementById('cart_items_counter')
+// let find_cart_length= document.getElementsByClassName('cart_item_quantity') 
+// let cart_qnty_counter=0
+// for (let q = 0; q < find_cart_length.length; q++) {
+ 
+//   let t = find_cart_length[q].innerHTML;
+//   window.alert(t)
+//   cart_qnty_counter=cart_qnty_counter+ Number(t)
+//  cart_length.innerHTML = cart_qnty_counter
+ 
+// }
+let counter=0
+for (let index = 0; index < shopping_cart.length; index++) {
+  let in_cart_item = shopping_cart[index].quantity
+counter=counter+in_cart_item
+  cart_length.innerHTML=counter;
+  
+}}
+////////////////////////try here paging
+// let get_page_number= document.getElementById('forward').addEventListener('click', goforward)
+// function goforward() {
+  
+//   let get_renderboxx=document.getElementsByClassName(' product_box')
+//   for (let index = 0; index < get_renderboxx.length; index++) {
+//     let t = get_renderboxx[index]
+//     t.innerHTML="";
+
+//   }
+  
+  
+//   for (let y = 4; y < products.length; y++) {
+//     const element = products[y];
+//   var ull = document.getElementById('products_catalogue_box')
+//   let lii= document.createElement('ul');
+//   lii.className= "product_box"
+  
+//   lii.innerHTML = `
+        
+//   <li class="artikul">${element.id}</li>
+//   <li > <img src= ${element.img} width="500" height="500" class= "product_image_zoom"> </li>
+//     <li>${element.name}</li>
+//     <li>${element.price}   $</li>
+//     <li class="get_id"><button class="c cursor">PLACE IN CART</button></li>
+  
+  
+//   `;
+//   ull.appendChild(lii);
+
+// }
+
+// }
+
+  
+
